9dba6d5e5234074bf8f584cead9ed3902d0fcc87
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-10-07 04:10:44 +0900

    Update main_screen.dart
    - added missing modification

--- instance_monitor/lib/screens/main_screen.dart
+++ instance_monitor/lib/screens/main_screen.dart
@@ -10,7 +10,28 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: LayeredGraph(),
+      body: LayeredGraph(topology: example),
     );
   }
 }
+
+var example = {
+  "nodes": [
+    {"id": 1, "label": 'circle'},
+    {"id": 2, "label": 'ellipse'},
+    {"id": 3, "label": 'database'},
+    {"id": 4, "label": 'box'},
+    {"id": 5, "label": 'diamond'},
+    {"id": 6, "label": 'dot'},
+    {"id": 7, "label": 'square'},
+    {"id": 8, "label": 'triangle'},
+  ],
+  "edges": [
+    {"from": 1, "to": 2},
+    {"from": 2, "to": 3},
+    {"from": 2, "to": 4},
+    {"from": 2, "to": 5},
+    {"from": 5, "to": 6},
+    {"from": 5, "to": 7},
+  ]
+};
