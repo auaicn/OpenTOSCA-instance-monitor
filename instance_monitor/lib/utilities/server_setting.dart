e8501a152d3ad1ba63a514b6b403620f71b19f80
Kyung Ho, Park <crorowslowpp@gmail.com>
2021-10-23 01:13:28 +0900

    Feat: changing baseUri

--- instance_monitor/lib/utilities/server_setting.dart
+++ instance_monitor/lib/utilities/server_setting.dart
@@ -1,5 +1,6 @@
 // TODO - change with localhost
-String baseUri = 'http://ec2-13-124-245-193.ap-northeast-2.compute.amazonaws.com';
+String baseUri = 'http://localhost';
+
 int backendServerPort = 8000;
 int dockerEngintPort = 2220;
 String backendServerUri = '$baseUri:$backendServerPort';
